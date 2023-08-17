66f30ad9c976e5fef74396ad06d41cbe79418756
skokdmitriy <84864953+skokdmitriy@users.noreply.github.com>
2023-08-17 21:50:01 +0700

    Initial Commit

---
+++ Rick & Morty 1221/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick & Morty 1221
+//
+//  Created by Дмитрий Скок on 17.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
